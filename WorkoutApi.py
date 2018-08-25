687bc45854b2f719e8260cd77c53c958fb6a4180
dimmxx <dimmxx@gmail.com>
2018-08-25 18:56:50 +0300

    25.08.18

--- WorkoutApi.py
+++ WorkoutApi.py
@@ -1,9 +1,9 @@
 import requests
 
+
 class ServerApi():
 
-    #display workouts by exceciserUuid
-
+    # display workouts by exceciserUuid
     def workoutByExceciserUuid (self, token, uuid, categoryId, modifiedAt, limit, page):
 
         headers = {'content-type': 'application/json', 'access_token': token, }
@@ -15,13 +15,9 @@ class ServerApi():
                 + '&limit=' + str(limit), headers=headers)
         return r
 
-
-
-    #display workouts by workoutId
-
+    # display workouts by workoutId
     def workoutByWorkoutId (self, token, workoutId):
 
         headers = {'content-type': 'application/json', 'access_token': token, }
-
         r = requests.get('https://api.netpulse.com/np/workouts-partner/v1.0/workouts/' + str(workoutId), headers=headers)
         return r
